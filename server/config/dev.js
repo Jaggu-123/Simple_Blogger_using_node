a675f937597e7f5a52322300d3828cbf3f8d0f68
Jaggu <khushboojagnani254@gmail.com>
2018-12-22 14:36:46 +0530

    setup for google auth

---
+++ server/config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    googleClientID:
+        "719109373239-2vf2o4ss40lnhrvo39np7jnq7ql1s1p7.apps.googleusercontent.com",
+    googleClientSecret: "vNkdwXvTkVAT184o1QTaTavl"
+};
---
+++ server/services/passport.js
@@ -0,0 +1,17 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("../config/dev");
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: "/auth/google/callback",
+            proxy: true
+        },
+        (accessToken, refreshToken, profile, done) => {
+            console.log(accessToken);
+        }
+    )
+);
